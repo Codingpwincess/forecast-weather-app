645b1ad0a73f63d4b4d20dde1208171e7ecf2ed9
Lunathi Mihlali Ndlondlo <130494106+Mimii3@users.noreply.github.com>
2025-05-31 19:36:44 +0200

    updated footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
         <Weather defaultCity="Port Elizabeth," defaultCountry="RSA"/>
         <footer>
+          <p>Copyright &#169; 2025 Lunathi Mihlali Ndlondlo. All rights reserved.</p>
           This app was built by <a href="https://www.github.com/codingpwincess">Lunathi M. Ndlondlo</a> using React hosted on <a href="https://www.netlify.com/codingpwincess">Netlify</a>
         </footer>
     </div>
